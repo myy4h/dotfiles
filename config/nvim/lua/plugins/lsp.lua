ee195eb5dd701740e6b143c7facc7c66a546a773
myy4h <mayabouchema@proton.me>
2026-09-19 20:25:16 +0200

    add mpv

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,6 @@
 return {
     "neovim/nvim-lspconfig",
     {
-        vim.lsp.enable({ "lua_ls", "nixd" }),
+        vim.lsp.enable({ "lua_ls", "nixd", "ts_ls", "html", "tailwindcss", "rust_analyzer", "svelte", "clangd", "jdtls" }),
     }
 }
